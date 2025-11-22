3da99b44011bcfc0fbdf0dabeee565df32875156
allankipkogei <122667985+allankipkogei@users.noreply.github.com>
2025-11-22 11:59:57 +0300

    Refactor: Rename main project folder to core

--- construction_erp/backend/core/settings.py
+++ construction_erp/backend/core/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings
 SECRET_KEY = 'django-insecure-l&mabazw!$0%n0-+79e0ty-7uv&_djy613dp!_cd6u(zpgr6z='
